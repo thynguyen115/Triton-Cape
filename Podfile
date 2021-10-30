ac40c93dfc70158409685b7b62d9cfcdd704c2b5
thynguyen115 <77253286+thynguyen115@users.noreply.github.com>
2021-10-29 23:59:31 -0700

    Add files via upload

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Triton-Cape' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Triton-Cape
+  pod 'Parse'
+  pod 'AlamofireImage'
+  pod 'MessageInputBar'
+
+  target 'Triton-CapeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Triton-CapeUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ Triton-Cape/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  Triton-Cape
+//
+//  Created by Triton-Cape Group on 10/26/21.
+//
+
+import UIKit
+import Parse
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    //var window:UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "67a8DtqhQxsSHjUeWX1K6fE4nIYqqHVJwm40vzNL" // <- UPDATE
+                $0.clientKey = "bAZLFxDL5ebg7VLCRy0B4f6MkIq3IKAD1dSrxxK7" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
